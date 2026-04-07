bde89f951ec14841a489c3ab8972433a711f51a8
E1win <87700553+E1win@users.noreply.github.com>
2026-04-07 15:04:16 +0200

    Created organisation

---
+++ Domain/Entities/Organisation.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class Organisation
+    {
+        public required Guid Id { get; set; }
+        [MaxLength(256)]
+        public required string Name { get; set; }
+        [MaxLength(8)]
+        public required string KvkNumber { get; set; }
+        public required OrganisationType OrganisationType { get; set; }
+
+    }
+}
