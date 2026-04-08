cc96967dbcd0aff85c15cf2e75f7128a73bf44d3
E1win <87700553+E1win@users.noreply.github.com>
2026-04-08 17:23:46 +0200

    Refactored getting of current user and role into own service

---
+++ Application/Interfaces/Services/ICurrentUserService.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces.Services;
+
+public interface ICurrentUserService
+{
+    public Task<(ApplicationUser User, string Role)> GetUserWithRoleAsync(string userId);
+}
---
+++ Application/Services/CurrentUserService.cs
@@ -0,0 +1,20 @@
+﻿using Application.Interfaces.Services;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Services;
+
+public class CurrentUserService(UserManager<ApplicationUser> userManager) : ICurrentUserService
+{
+    public async Task<(ApplicationUser User, string Role)> GetUserWithRoleAsync(string userId)
+    {
+        var user = await userManager.FindByIdAsync(userId)
+            ?? throw new InvalidOperationException("User not found.");
+
+        var roles = await userManager.GetRolesAsync(user);
+        var role = roles.FirstOrDefault()
+            ?? throw new InvalidOperationException("User has no role assigned.");
+
+        return (user, role);
+    }
+}
