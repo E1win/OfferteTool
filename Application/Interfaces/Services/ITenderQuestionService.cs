ad84c72b387340897cddec670e11afbfd37b62e5
E1win <87700553+E1win@users.noreply.github.com>
2026-04-13 09:28:21 +0200

    Added method to update question

--- Application/Interfaces/Services/ITenderQuestionService.cs
+++ Application/Interfaces/Services/ITenderQuestionService.cs
@@ -5,4 +5,5 @@ namespace Application.Interfaces.Services;
 public interface ITenderQuestionService
 {
     Task<TenderQuestion> CreateQuestionAsync(Guid tenderId, TenderQuestion question, string userId);
+    Task<TenderQuestion> UpdateQuestionAsync(Guid questionId, TenderQuestion updatedQuestion, string userId);
 }
--- Application/Services/TenderQuestionService.cs
+++ Application/Services/TenderQuestionService.cs
@@ -34,4 +34,36 @@ public class TenderQuestionService(ITenderRepository tenderRepository, ICurrentU
 
         return await tenderQuestionRepository.AddAsync(question);
     }
+
+    public async Task<TenderQuestion> UpdateQuestionAsync(Guid questionId, TenderQuestion updatedQuestion, string userId)
+    {
+        TenderQuestion existingQuestion = await tenderQuestionRepository.GetByIdAsync(questionId)
+            ?? throw new KeyNotFoundException("Question not found.");
+
+        Tender tender = await tenderRepository.GetByIdAsync(existingQuestion.TenderId)
+            ?? throw new KeyNotFoundException("Tender not found.");
+
+        var (user, role) = await currentUserService.GetUserWithRoleAsync(userId);
+
+        if (!tender.IsAccessibleBy(user, role))
+            throw new UnauthorizedAccessException("User does not have access to this tender.");
+
+        if (role != Roles.Inkoper)
+            throw new UnauthorizedAccessException("Only inkopers can update questions.");
+
+        if (!tender.CanBeEdited())
+            throw new InvalidOperationException("Questions can only be changed while the tender is in Design.");
+
+        existingQuestion.Text = updatedQuestion.Text;
+        existingQuestion.Score = updatedQuestion.Score;
+        if (existingQuestion is ChoiceQuestion existingChoice && updatedQuestion is ChoiceQuestion updatedChoice)
+        {
+            existingChoice.SetOptions([.. updatedChoice.Options]);
+        }
+        existingQuestion.Validate();
+
+        await tenderQuestionRepository.SaveChangesAsync();
+
+        return existingQuestion;
+    }
 }
