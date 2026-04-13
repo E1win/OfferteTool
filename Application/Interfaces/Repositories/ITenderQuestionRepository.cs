9cc0d3fe7bce51e433fe51f9b97d6b1edec7425e
E1win <87700553+E1win@users.noreply.github.com>
2026-04-13 08:41:56 +0200

    Added TenderQuestionRepository

---
+++ Application/Interfaces/Repositories/ITenderQuestionRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities.TenderQuestions;
+
+namespace Application.Interfaces.Repositories;
+
+public interface ITenderQuestionRepository
+{
+    Task<TenderQuestion?> GetByIdAsync(Guid id);
+    Task<TenderQuestion> AddAsync(TenderQuestion question);
+    Task DeleteAsync(TenderQuestion question);
+    Task SaveChangesAsync();
+}
---
+++ Infrastructure/Repositories/TenderQuestionRepository.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities.TenderQuestions;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class TenderQuestionRepository(AppDbContext dbContext) : ITenderQuestionRepository
+{
+    public async Task<TenderQuestion?> GetByIdAsync(Guid id) =>
+        await dbContext.Set<TenderQuestion>()
+            .Include(q => ((ChoiceQuestion)q).Options)
+            .FirstOrDefaultAsync(q => q.Id == id);
+
+    public async Task<TenderQuestion> AddAsync(TenderQuestion question)
+    {
+        dbContext.Set<TenderQuestion>().Add(question);
+        await dbContext.SaveChangesAsync();
+        return question;
+    }
+
+    public async Task DeleteAsync(TenderQuestion question)
+    {
+        dbContext.Set<TenderQuestion>().Remove(question);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await dbContext.SaveChangesAsync();
+    }
+}
