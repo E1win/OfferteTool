51f4c84da7e689d31c5160a37ebaa88c112a0299
E1win <87700553+E1win@users.noreply.github.com>
2026-04-25 10:19:15 +0200

    No longer retrieve choiceAnswer from database

--- Infrastructure/Repositories/TenderSubmissionRepository.cs
+++ Infrastructure/Repositories/TenderSubmissionRepository.cs
@@ -1,6 +1,5 @@
 using Application.Interfaces.Repositories;
 using Domain.Entities;
-using Domain.Entities.TenderAnswers;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,7 +10,6 @@ public class TenderSubmissionRepository(AppDbContext dbContext) : ITenderSubmiss
     public async Task<TenderSubmission?> GetByTenderAndSupplierAsync(Guid tenderId, Guid supplierId) =>
         await dbContext.TenderSubmissions
             .Include(submission => submission.Answers)
-            .ThenInclude(answer => ((ChoiceAnswer)answer).Selections)
             .FirstOrDefaultAsync(submission => submission.TenderId == tenderId && submission.SupplierId == supplierId);
 
     public async Task<TenderSubmission> AddAsync(TenderSubmission submission)
