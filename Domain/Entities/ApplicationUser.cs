c6419c0752b92002637caf86ac54f2ce5b4c4716
E1win <87700553+E1win@users.noreply.github.com>
2026-05-04 11:46:16 +0200

    Added IsActive flag to ApplicationUser

--- Domain/Entities/ApplicationUser.cs
+++ Domain/Entities/ApplicationUser.cs
@@ -15,6 +15,7 @@ public class ApplicationUser : IdentityUser
 
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
+    public bool IsActive { get; set; } = true;
 
     public Guid? OrganisationId { get; set; }
     public Organisation? Organisation { get; set; }
