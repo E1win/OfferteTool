b8286ccdcd563d94afdc755a888c858f16488eaa
E1win <87700553+E1win@users.noreply.github.com>
2026-05-12 09:29:11 +0200

    Setup E2E testing project

---
+++ Tests.E2E/BasePageTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.Xunit;
+
+namespace Tests.E2E
+{
+    public class BasePageTest : PageTest
+    {
+        public override BrowserNewContextOptions ContextOptions()
+        {
+            return new BrowserNewContextOptions()
+            {
+                ViewportSize = new()
+                {
+                    Width = 1920,
+                    Height = 1080
+                },
+                // So we only need to use relative URLs in the tests
+                BaseURL = "https://localhost:7018/",
+            };
+        }
+    }
+}
---
+++ Tests.E2E/LoginTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Tests.E2E
+{
+    public class LoginTests : BasePageTest
+    {
+        [Fact]
+        public async Task LoginPageLoads()
+        {
+            await Page.GotoAsync("/Identity/Account/Login");
+        }
+    }
+}
